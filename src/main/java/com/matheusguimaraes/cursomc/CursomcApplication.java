5f323b540ac24a755a3e4cbb9477b8295534bf22
Matheus Guimaraes <matheusgarciabsb@gmail.com>
2018-11-17 16:01:12 -0200

    REST TESTADO

--- src/main/java/com/matheusguimaraes/cursomc/CursomcApplication.java
+++ src/main/java/com/matheusguimaraes/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.matheusguimaraes;
+package com.matheusguimaraes.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
