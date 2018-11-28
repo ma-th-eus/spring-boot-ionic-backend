6a3a85907fab910ffc1d480424bbb623cbedd6d9
Matheus Guimaraes <matheusgarciabsb@gmail.com>
2018-11-28 20:19:21 -0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/matheusguimaraes/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.matheusguimaraes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusguimaraes.cursomc.domain.Cliente;
+import com.matheusguimaraes.cursomc.repositories.ClienteRepository;
+import com.matheusguimaraes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
+
