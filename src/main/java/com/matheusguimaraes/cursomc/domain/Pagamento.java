9b4f5d9aabe2a72d09e66095b2b6a103462d3d8f
Matheus Guimaraes <matheusgarciabsb@gmail.com>
2018-12-14 02:45:04 -0200

    Inserindo Pedido

--- src/main/java/com/matheusguimaraes/cursomc/domain/Pagamento.java
+++ src/main/java/com/matheusguimaraes/cursomc/domain/Pagamento.java
@@ -11,10 +11,12 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.matheusguimaraes.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")	
 public abstract class Pagamento implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -51,7 +53,7 @@ public abstract class Pagamento implements Serializable{
 		return EstadoPagamento.toEnum(estado);
 	}
 
-	public void setEstadoPagamento(EstadoPagamento estado) {
+	public void setEstado(EstadoPagamento estado) {
 		this.estado = estado.getCod();
 	}
 
--- src/main/java/com/matheusguimaraes/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/matheusguimaraes/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.matheusguimaraes.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.matheusguimaraes.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	
 	private static final long serialVersionUID = 1L;
